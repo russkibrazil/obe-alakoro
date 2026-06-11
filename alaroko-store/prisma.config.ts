09f9a1b092c5560ecd39c74b8b702442836d66a7
Igor Pereira <igorp9971@gmail.com>
2026-06-11 09:34:03 -0300

    Split Docker dev env

--- alaroko-store/prisma.config.ts
+++ alaroko-store/prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: process.env["DATABASE_URL"] ?? "mysql://user_app:user_password_here@db:3306/loja_virtual"
   },
 });
